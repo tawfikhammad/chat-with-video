7b5ac2f89771b4101b95808e401412da4436a0cd
tawfikhammad <tawfikhammad@icloud.com>
2025-05-01 15:51:32 +0300

    update app settings

--- src/utils/app_config.py
+++ src/utils/app_config.py
@@ -4,6 +4,9 @@ class settings(BaseSettings):
 
     APP_NAME: str 
     APP_VERSION: str 
+    YOUTUBE_API: str
+    MONGO_URL: str
+    MONGO_DB: str = "vidbot"
 
     class Config:
         env_file = "src/.env"
