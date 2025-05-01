b3a63ffb632aa643bd14e95c5d55e1fe95ecb543
tawfikhammad <tawfikhammad@icloud.com>
2025-05-01 15:55:55 +0300

    create process controller (get chunks)

---
+++ src/controllers/process_controller.py
@@ -0,0 +1,24 @@
+from .BaseController import BaseController
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+class TextProcessor(BaseController):
+    def __init__(self):
+        super().__init__()
+
+    def transcript_chunks(self, transcript: list, chunk_size:int= 500, chunk_overlap:int= 150) -> str:
+
+        strings = []
+        for item in transcript:
+            strings.append(f"{item['start']}: {item['text']}")
+
+        strings = '\n'.join(strings)
+
+        text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+        is_separator_regex=False,)
+
+        chunks = text_splitter.create_documents([strings])
+
+        return chunks    
