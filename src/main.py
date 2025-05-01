6c673a3737a8459410b0880ed033c8483907561b
tawfikhammad <tawfikhammad@icloud.com>
2025-05-01 15:53:27 +0300

    update main file (connect to mongodb)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
-from routes import base
+from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from utils.app_config import get_settings
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    settings = get_settings()
+    app.mongodb_conn = AsyncIOMotorClient(settings.MONGO_URL)
+    app.mongodb_client = app.mongodb_conn[settings.MONGO_DB]
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.mongodb_conn.close()
+
 app.include_router(base.base_router)
+app.include_router(data.data_router)
 
 
 if __name__ == "__main__":
