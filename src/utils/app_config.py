a229c5bc6f6360ce3dfe69a7e5198a0ff4526251
tawfikhammad <tawfikhammad@icloud.com>
2025-05-18 22:25:21 +0300

    add llms and vdb config

--- src/utils/app_config.py
+++ src/utils/app_config.py
@@ -8,8 +8,31 @@ class settings(BaseSettings):
     MONGO_URL: str
     MONGO_DB: str = "vidbot"
 
+    GENERATION_BACKEND : str
+    EMBEDDING_BACKEND : str
+
+    OPENAI_API_KEY: str
+    OPENAI_API_URL: str
+    COHERE_API_KEY: str
+    GEMINI_API_KEY: str
+
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_MODEL_SIZE: int 
+
+    INPUT_DAFAULT_MAX_CHARACTERS: int
+    GENERATION_DAFAULT_MAX_TOKENS: int
+    GENERATION_DAFAULT_TEMPERATURE: int
+
+    VECTOR_DB_BACKEND : str
+    VECTOR_DB_PATH : str
+    VECTOR_DB_DISTANCE_METHOD : str
+
+    PRIMARY_LANG : str = "en"
+    DEFAULT_LANG : str = "en"
+
     class Config:
-        env_file = "src/.env"
+        env_file= "src/.env"
 
 def get_settings() -> settings:
     return settings()
