bcba14d378f1b17526f88fca7ef91fbab2725273
tawfikhammad <tawfikhammad@icloud.com>
2025-05-19 00:18:35 +0300

    modify main file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
-from routes import base, data
+from routes import base, data, rag
 from motor.motor_asyncio import AsyncIOMotorClient
 from utils.app_config import get_settings
+from AI.VectorDB.VDBFactory import VDBFactory
+from AI.LLM.LLMFactory import LLMProviderFactory
+from AI.LLM.templates import TemplateParser
 
 app = FastAPI()
 
@@ -12,12 +15,35 @@ async def startup():
     app.mongodb_conn = AsyncIOMotorClient(settings.MONGO_URL)
     app.mongodb_client = app.mongodb_conn[settings.MONGO_DB]
 
+    llm_provider_factory = LLMProviderFactory(settings)
+    vdb_provider_factory = VDBFactory(settings)
+
+    # generation client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    
+    # vector db client
+    app.vectordb_client = vdb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
+
+    app.template_parser = TemplateParser(
+        language=settings.PRIMARY_LANG,
+        default_language=settings.DEFAULT_LANG,
+    )
+
 @app.on_event("shutdown")
 async def shutdown():
     app.mongodb_conn.close()
+    app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
-app.include_router(data.data_router)
+app.include_router(data.data_router, prefix="/data", tags=["Data"])
+app.include_router(rag.rag_router, prefix="/{video_id}", tags=["RAG"])
 
 
 if __name__ == "__main__":
